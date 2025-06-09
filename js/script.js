43516586ddd1b0f6ed046c7fb3e8afb66e488321
cauansaile1 <cauansaile1@gmail.com>
2025-06-09 11:38:47 -0300

    finalizei formulário

--- js/script.js
+++ js/script.js
@@ -59,56 +59,23 @@ window.addEventListener('load', () => {
 });
 
 
+document.getElementById("formContato").addEventListener("submit", function(event) {
+event.preventDefault();
 
+  const nome = document.getElementById("nome").value;
+  const telefone = document.getElementById("numero").value;
+  const servico = document.getElementById("servico-desejado").value;
+  const endereco = document.getElementById("endereco").value;
+  const data = document.getElementById("data").value;
+  
 
+  const texto = `Olá! Gostaria de realizar um agendamento com Laleska Palmeira %0A Nome: ${nome}%0A Telefone: ${telefone}%0A Serviço desejado: ${servico}%0A Endereço: ${endereco}%0A Data: ${data}`;
 
+  const numero = "5571993322766"; // Altere para o número da escola com DDI + DDD
+  const url = `https://wa.me/${numero}?text=${texto}`;
 
-
-
-
-/*const track = document.getElementById('carousel-track');
-const items = Array.from(track.children);
-const itemWidth = items[0].offsetWidth + 10;
-const visibleItems = 3;
-
-// Clona os primeiros itens para o final (loop infinito)
-for (let i = 0; i < visibleItems; i++) {
-  track.appendChild(items[i].cloneNode(true));
-}
-
-let index = 0;
-
-function moveNext() {
-  index++;
-  track.style.transition = 'transform 0.5s ease';
-  track.style.transform = `translateX(-${index * itemWidth}px)`;
-
-  // Quando chega no clone, volta pro original
-  if (index === items.length) {
-    setTimeout(() => {
-      track.style.transition = 'none';
-      track.style.transform = 'translateX(0)';
-      index = 0;
-    }, 500); // igual ao tempo da transição
-  }
-}
-
-document.getElementById('nextBtn').addEventListener('click', moveNext);
-
-
-
-  // Swipe em celular
-let startX = 0;
-track.addEventListener('touchstart', e => startX = e.touches[0].clientX);
-track.addEventListener('touchend', e => {
-  const endX = e.changedTouches[0].clientX;
-  const diff = endX - startX;
-  if (Math.abs(diff) > 50) {
-    if (diff < 0) moveNext();
-    else movePrev();
-  }
+  window.open(url, "_blank");
 });
 
 
-// Auto avanço
-setInterval(moveNext, 3000);*/
+
